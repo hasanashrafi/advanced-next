8dd7997aec7a512b7ff54966dd2fd9eede72797d
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-04 18:09:56 +0330

    added head tag

---
+++ pages/next.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+function NextJs() {
+    return (
+        <div className='min-h-screen p-5 '>
+            <Head>
+                <title>Next Js</title>
+            </Head>
+            <h1>NextJs</h1>
+        </div>
+    )
+}
+
+export default NextJs
