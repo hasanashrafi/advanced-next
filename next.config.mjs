59fb8a6c99cbdd237ef115fbc979bf86fcf71e57
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-06 20:37:53 +0330

    config next setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      { source: "/js", destination: "/languages", permanent: true },
+      { source: "/react", destination: "/next", permanent: true },
+
+    ]
+  }
 };
 
 export default nextConfig;
