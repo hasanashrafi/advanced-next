f7225e50d93897aa49a79d0f2d3379b8f35ef7d0
Hasan Ashrafi <yasashrafi@gmail.com>
2024-07-12 19:33:38 +0330

    added page component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Page from "@/components/templates/Page";
 import Head from "next/head";
 
 
@@ -11,6 +12,7 @@ export default function Home() {
       </Head>
       <div className="z-10 max-w-5xl w-full items-center justify-between  text-sm lg:flex">
         <h1 className="text-indigo-600 text-4xl font-semibold font-mono">Next.Js Advanced </h1>
+        <Page />
       </div>
     </main>
   );
